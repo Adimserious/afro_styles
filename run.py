3e8406ab822bf9c42f97afca3ecce052c3442cf4
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-04-02 19:55:45 +0000

    Set up APIs and enable google sheet access

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import gspread library and credential class from google-auth library after download 
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Google Sheets API scope and credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("afro_styles")
+
+main_sales = SHEET.worksheet("main_sales")
+
+afro_data = main_sales.get_all_values()
+print(afro_data)
