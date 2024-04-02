a55363364faa6bee84d6522d4d895a37c308be95
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-04-02 21:32:03 +0000

    Create authentic_data function to check for exactly 4 rates

--- run.py
+++ run.py
@@ -18,13 +18,29 @@ main_sales = SHEET.worksheet("main_sales")
 
 def request_main_sales():
     """Main sales data request from user"""
+
     print("welcome to Afro_styles, please enter yesterday's main sales.")
     print("please enter four numbers, separated by commas.")
     print("for_instance: 20,1,44,5\n")
 
     last_sales = input("Enter last sales: ")
-    print(f"you entered {last_sales}")
+    
+    # split method to break commas from string data in a list
+    main_sales_data = last_sales.split(",")
+    
+    # call rates date from authentic_data function
+    authentic_data(main_sales_data)
 
-
+def authentic_data(rates):
+    """
+    try statement to coverts string rates to integer, raise valueError if not interger or 4 rates exactly
+    """
+    try:
+        if len(rates) != 4:
+            raise ValueError(
+                f"4 rates are required, you provided {len(rates)}"
+            )
+    except ValueError as e:
+        print(f"Invalid rates: {e}, please try again.\n")
 
 request_main_sales()
